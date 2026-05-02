b30a340395457517aa1bbbfbc580083ba414c5c4
Wang Xu <2889087473@qq.com>
2026-05-02 19:22:55 +0800

    自动备份 周六 2026/05/02 19:22:55.35

--- auto.bat
+++ auto.bat
@@ -21,25 +21,3 @@ git commit -m "自动备份 %date% %time%" >nul 2>&1
 git push origin yuanma >nul 2>&1
 echo 源码备份完成！
 echo.
-
-:: ============== 3. 清理缓存 ==============
-echo [3/5] 执行 hexo clean...
-hexo clean
-echo.
-
-:: ============== 4. 生成网站 ==============
-echo [4/5] 执行 hexo generate...
-hexo g
-echo.
-
-:: ============== 5. 部署网站 ==============
-echo [5/5] 执行 hexo deploy...
-hexo d
-echo.
-
-echo ==============================================
-echo              执行成功！🎉
-echo        源码已备份 | 网站已部署
-echo ==============================================
-pause
-exit
